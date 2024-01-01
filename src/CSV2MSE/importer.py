2b8ed5b6c45564f38c3cd5ef0c6806faec2be2ca
Avery Garon <averygaron@gmail.com>
2024-01-01 12:40:47 -0800

    Create config file

---
+++ src/CSV2MSE/importer.py
@@ -0,0 +1,21 @@
+import configparser
+
+def read_config_file():
+    config = configparser.ConfigParser()
+    config.read(file)
+
+def create_set_dir():
+    return
+
+def read_csv():
+    contents = None
+    return contents
+
+def process_csv(dict):
+    return
+
+if __name__ == "__main__":
+    # read config file
+    # initialize set directory and set file
+    # read csv file
+    # process csv file
