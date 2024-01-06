505fd688fe35e8e471ad065967ac4444d01e08f5
Avery Garon <averygaron@gmail.com>
2024-01-06 11:39:17 -0800

    Add visible error message

--- src/CSV2MSE/main.py
+++ src/CSV2MSE/main.py
@@ -1,9 +1,13 @@
 import card_importer
 
 if __name__ == "__main__":
-    config_file = "metadata.cfg"
-    metadata, columns = card_importer.read_config_file(config_file)
-    set_dir = card_importer.create_set_dir(metadata)
-    card_list = card_importer.read_csv()
-    card_importer.process_csv(set_dir, columns, card_list)
-    card_importer.zip_set_dir(set_dir)
+    try:
+        config_file = "metadata.cfg"
+        metadata, columns = card_importer.read_config_file(config_file)
+        set_dir = card_importer.create_set_dir(metadata)
+        card_list = card_importer.read_csv()
+        card_importer.process_csv(set_dir, columns, card_list)
+        card_importer.zip_set_dir(set_dir)
+    except Exception as e:
+        print(e)
+        input("Press any key to exit")
